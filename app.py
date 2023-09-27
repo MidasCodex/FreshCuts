92a2028238b470f975068c5f1bb9fcdf4c2a3555
MidasCodex <144654284+MidasCodex@users.noreply.github.com>
2023-09-27 12:32:37 +0300

    Further Visual Enhancement Added Features And Pages

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import secrets
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Import Flask-Migrate
-
+from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-# Configure your SQLAlchemy database (replace 'sqlite:///mydatabase.db' with your database URL).
+# Configure SQLAlchemy database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db = SQLAlchemy(app)
 
@@ -14,7 +14,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Secret key for session management (replace with a secure, random key).
-app.secret_key = 'your_secret_key'
+app.secret_key = secrets.token_hex(16)  # Generate a secure secret key
+
 
 # Define a User model for the database.
 class User(db.Model):
@@ -22,9 +23,19 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
+
+# Define an Admin model for the database.
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+
 # Create the database tables within the application context.
 with app.app_context():
     db.create_all()
+
 
 # Route for user registration.
 @app.route('/register', methods=['GET', 'POST'])
@@ -37,20 +48,20 @@ def register():
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             flash('Username already exists. Please choose a different one.', 'danger')
-            return redirect(url_for('register'))
+        else:
+            # Hash the user's password for security.
+            hashed_password = generate_password_hash(password, method='sha256')
 
-        # Hash the user's password for security.
-        hashed_password = generate_password_hash(password, method='sha256')
+            # Create a new user and add it to the database.
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
 
-        # Create a new user and add it to the database.
-        new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash('Registration successful. You can now log in.', 'success')
-        return redirect(url_for('login'))
+            flash('Registration successful. You can now log in.', 'success')
+            return redirect(url_for('login'))
 
     return render_template('register.html')
+
 
 # Route for user login.
 @app.route('/login', methods=['GET', 'POST'])
@@ -61,26 +72,36 @@ def login():
 
         # Check if the username exists in the database.
         user = User.query.filter_by(username=username).first()
-        if not user:
-            flash('Invalid username or password. Please try again.', 'danger')
-            return redirect(url_for('login'))
-
-        # Verify the password using the stored hash.
-        if check_password_hash(user.password, password):
+        if user and check_password_hash(user.password, password):
             # Store the user's ID in the session to track the logged-in user.
             session['user_id'] = user.id
             flash('Login successful.', 'success')
             return redirect(url_for('home'))
-        else:
-            flash('Invalid username or password. Please try again.', 'danger')
+
+        flash('Invalid username or password. Please try again.', 'danger')
 
     return render_template('login.html')
+
 
 # Route for homepage.
 @app.route('/home')
 def home():
-    return "Welcome to the home page!"
+    return render_template('home.html')
 
+# Route for aboutpage.
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for faq.
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+# Route for faq.
+@app.route('/tos')
+def tos():
+    return render_template('tos.html')
 # Route for user logout.
 @app.route('/logout')
 def logout():
@@ -89,13 +110,71 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
+
 # Route for the root URL ("/") to display the "App Running" message.
 @app.route('/')
 def app_running():
     return render_template('app_running.html')
 
 
-# ... more routes and functionality ...
+# Admin registration route.
+@app.route('/admin/register', methods=['GET', 'POST'])
+def admin_register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username already exists in the database.
+        existing_admin = Admin.query.filter_by(username=username).first()
+        if existing_admin:
+            flash('Admin username already exists. Please choose a different one.', 'danger')
+        else:
+            # Hash the admin's password for security.
+            hashed_password = generate_password_hash(password, method='sha256')
+
+            # Create a new admin and add it to the database.
+            new_admin = Admin(username=username, password=hashed_password, is_admin=True)
+            db.session.add(new_admin)
+            db.session.commit()
+
+            flash('Admin registration successful. You can now log in as an admin.', 'success')
+            return redirect(url_for('admin_login'))
+
+    return render_template('admin_register.html')
+
+
+# Admin login route.
+@app.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username exists in the database.
+        admin = Admin.query.filter_by(username=username).first()
+        if admin and check_password_hash(admin.password, password):
+            if admin.is_admin:
+                session['admin_id'] = admin.id
+                flash('Admin login successful.', 'success')
+                return redirect(url_for('admin_dashboard'))
+            else:
+                flash('You do not have permission to log in as an admin.', 'danger')
+        else:
+            flash('Invalid admin username or password. Please try again.', 'danger')
+
+    return render_template('admin_login.html')
+
+
+# Admin dashboard route.
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    if 'admin_id' in session:
+        # Add logic here to oversee users or perform admin actions as needed
+        return render_template('admin_dashboard.html')
+    else:
+        flash('You need to log in as an admin to access the admin dashboard.', 'danger')
+        return redirect(url_for('admin_login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
